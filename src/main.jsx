11f9fefc29d4c71e176d95b4c11fb7c2afb7a077
unknown <mukundh2019@gmail.com>
2026-07-13 12:18:16 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import App from './App'
 import './index.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
